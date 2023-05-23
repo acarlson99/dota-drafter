56e9d5ab224040e3b964664dad7868d0d674f76b
Alexander <bombblob@gmail.com>
2023-05-23 11:34:20 -0700

    fix recursive doc creation

--- proj.el
+++ proj.el
@@ -1,15 +1,18 @@
+;; This file configures org mode's publish functionality to generate the gh-pages site for this project.
+;; Load this file/buffer, evaluate it, and then execute: M-x org-publish-project RET gh-pages RET
+
+(defvar proj-dir (file-name-directory (buffer-file-name)))
+
 (setq org-publish-project-alist
-	  '(("draml-notes"
-		 :base-directory "~/p/dota-draml/"
+	  `(("gh-notes"
+		 :base-directory ,proj-dir
 		 :base-extension "org"
 		 :publishing-function org-html-publish-to-html
-		 :publishing-directory "./docs"
-		 :recursive t
-		 )
-		("draml-assets"
-		 :base-directory "~/p/dota-draml/"
-		 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-		 :publishing-directory "./docs"
-		 :publishing-function org-publish-attachment
+		 :publishing-directory ,(string-join `(,proj-dir "docs"))
 		 :recursive t)
-		("dota-draml" :components ("draml-assets" "draml-notes"))))
+		("gh-assets"
+		 :base-directory ,proj-dir
+		 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+		 :publishing-directory ,(string-join `(,proj-dir "docs"))
+		 :publishing-function org-publish-attachment)
+		("gh-pages" :components ("gh-assets" "gh-notes"))))
